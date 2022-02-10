f929767122afdad5702ba12992502e5f92722618
Rajin <rajink92@gmail.com>
2022-02-09 19:28:37 -0500

    findkey

---
+++ letterPositions.js
@@ -0,0 +1,31 @@
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (const item of allItems) {
+    console.log(item);
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+}
+return results
+}
+
+const sentance = "Lighthouse in the house"
+
+const countLetters = function(text) {
+  let amount = text.length
+  let letters = []
+  for(let i = 0; i < amount; i++)  {
+    if( text.charAt(i) !== " ") {
+    letters.push(text.charAt(i));
+    }
+  }
+let result = countOnly(letters);
+return result;
+};
+console.log(countLetters(sentance));
+
+
+  return results;
