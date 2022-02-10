6fbe19cd69553adc95c017da66394749f56275e5
Rajin <rajink92@gmail.com>
2022-02-09 19:40:50 -0500

    eqobjects

---
+++ eqObjects.js
@@ -0,0 +1,17 @@
+const eqObjects = function(object1, object2) {
+  Object.keys(ab)
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+eqObjects(ab, ba); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+eqObjects(ab, abc);
+
+onst cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+eqObjects(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+eqObjects(cd, cd2); /
