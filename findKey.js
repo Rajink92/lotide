e10be0a49a8ba22dae9f9a529509ea2fe16565b3
Rajin <rajink92@gmail.com>
2022-02-12 21:43:57 -0500

    findkey

---
+++ findKey.js
@@ -0,0 +1,11 @@
+
+
+findKey(names{
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+} ,
+_.findKey(names, {stars: 1}) (x => x.stars === 2))
