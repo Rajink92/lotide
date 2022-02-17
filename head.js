3679c8a8722fa9564bd216def16ffc2ea2e2b4e2
Rajin <rajink92@gmail.com>
2022-02-16 19:02:23 -0500

    head

--- head.js
+++ head.js
@@ -1,8 +1,15 @@
-let ar = [5,6,7]
+const head = function (array) {
+  return array[0]
+};
 
-let first = tail.ar;
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else { 
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`)
+  }
+}
 
-console.log(first)
 
 
 // TEST CODE
--- tail.js
+++ tail.js
@@ -1,14 +1,14 @@
-let ar = [5,6,7]
-
-let first = tail.ar;
-
-console.log(first)
-
-
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
+const tail = function (array) {
+ let words = array.slice(1)
+ return words
+}
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  } else { 
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`)
+  }
+}
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(words.length, 3);
