3b46391bacdf27acebaa4e9323be91482858d9e5
Rajin <rajink92@gmail.com>
2022-02-19 13:28:45 -0500

    eqObjects

--- eqObjects.js
+++ eqObjects.js
@@ -1,17 +1,35 @@
+function eqArrays(a, b) {
+  let isThisEqual = null;
+  for (let index = 0; index < a.length; index++)  {
+    if (a[index] === b[index])  {
+      isThisEqual = true
+    }
+    else {
+      isThisEqual = false
+      return isThisEqual
+    }
+  }
+  return isThisEqual
+}
 const eqObjects = function(object1, object2) {
-  Object.keys(ab)
-};
-
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-eqObjects(ab, ba); // => true
-
-const abc = { a: "1", b: "2", c: "3" };
-eqObjects(ab, abc);
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-eqObjects(cd, dc); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-eqObjects(cd, cd2); /
+  if (Object.keys(object1).length !== Object.keys(object2).length)  {
+    return false; //edgecase if lengths are not equal, and converts to array
+  }
+  for (let key of Object.keys(object1)) {
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key]))  {
+        return false
+      }
+    }
+    if (object1[key] instanceof Object && object2[key] instanceof Object) {
+      if(!eqObjects(object1[key], object2[key]))  {
+        return false;
+      }
+    } else {
+      if (object1[key] !== object2[key])  {
+        return false;
+      }
+  }
+} 
+return true;
+}; 
