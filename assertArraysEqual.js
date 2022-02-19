9848bdb7234cef2d3c908aeb5b7f4102e08288e9
Rajin <rajink92@gmail.com>
2022-02-19 16:12:51 -0500

    takeuntil

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,23 @@
-function eqArrays(a, b) {
-  for (let index = 0; index < a.length; index++)  {
-    if (a[index] !== b[index])  {
-      return false
+const assertArraysEqual = function(actual, expected) {
+
+  if (eqArrays(actual, expected)) {
+    console.log(`:) Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`:( Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
     }
   }
-  return true
-}
-const assertEqual = function(a, b) {
-  if (a === b) {
-    console.log(`✅✅✅ Assertion Passed: ${a} === ${b}`)
-  } else { 
-    console.log(`🛑🛑🛑 Assertion Failed: ${a} === ${b}`)
-  }
-}
+  return true;
+};
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)  // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
--- map.js
+++ map.js
@@ -1,4 +1,25 @@
-const words = ["ground", "control", "to", "major", "tom"];
+const assertArraysEqual = function(actual, expected) {
+
+  if (eqArrays(actual, expected)) {
+    console.log(`:) Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`:( Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  } else {
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
 const map = function(array, callback) {
   const results = [];
   for (let item of array) {
@@ -6,3 +27,4 @@ const map = function(array, callback) {
   }
   return results;
 }
+const words = ["ground", "control", "to", "major", "tom"];
