601f48febcd0b6b541c72e24b9573d8002202727
Rajin <rajink92@gmail.com>
2022-02-22 15:43:56 -0500

    refractMiddle

--- middle.js
+++ middle.js
@@ -1,25 +1,11 @@
-//actual function
-const array1 = ([1, 2, 3]) // => [2]
-//console.log(array1)
-//middle([1, 2, 3, 4, 5]) // => [3]
-const midElement = Math.floor(array1.length / 2);
-const middle = function(array1)  {
-  //console.log(array1)
-  for( elements of array1 ) {
-    let arrayLength = array1.length;
-    if (arrayLength % 2 != 0)  {
-      console.log(array1[midElement])
-    }
-    else {
-      console.log([])
-    }
+const middle = function(array) {
+  if (array.length <= 2) {
+    return [];
+  } else if (array.length % 2 === 0) {
+    return [array[Math.ceil(array.length / 2) - 1], array[Math.ceil(array.length / 2)]];
+  } else {
+    return [array[Math.floor(array.length / 2)]];
   }
-}
-middle(array1)
+};
 
-//loop though array
-//get total number of elements in array
-// see if that number is NOT divisible by 2
-// if it does return the middle value
-//else return empty array
-
+module.exports = middle;
