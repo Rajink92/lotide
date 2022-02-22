f1dbbb8bfd27be0323f7fa1ca278464d629fc910
Rajin <rajink92@gmail.com>
2022-02-22 13:36:53 -0500

    tailHeadassertEqualRefractorerd

--- tail.js
+++ tail.js
@@ -1,14 +1,8 @@
+const assertEqual = require('./assertEqual')
+
 const tail = function (array) {
  let words = array.slice(1)
  return words
 }
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-  } else { 
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} === ${expected}`)
-  }
-}
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+
+module.exports = tail;
