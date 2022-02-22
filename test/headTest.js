d1bc3ccb5e80670feb1ddcc3bbe30acfbd9ab576
Rajin <rajink92@gmail.com>
2022-02-22 16:06:12 -0500

    MChead

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,23 @@
-const head = require('../head')
-const assertEqual = require('../assertEqual')
+const assert = require('chai').assert;
+const head   = require('../head');
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-head(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// const head = require('../head')
+// const assertEqual = require('../assertEqual')
+
+// // TEST CODE
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// head(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
