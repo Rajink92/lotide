275f2cb3518ee7460f838ea8c0d5fcdf83a371d3
Rajin <rajink92@gmail.com>
2022-02-22 14:49:42 -0500

    refractoreedAssertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,5 @@
+const eqArrays = require('./eqArrays')
+
 const assertArraysEqual = function(actual, expected) {
 
   if (eqArrays(actual, expected)) {
@@ -7,17 +9,5 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(actual, expected) {
-  if (actual.length !== expected.length) {
-    return false;
-  } else {
-    for (let i = 0; i < actual.length; i++) {
-      if (actual[i] !== expected[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true)  // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+module.exports = assertArraysEqual
+
