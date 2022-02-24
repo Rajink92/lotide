7ab72e9390dd636c47c667e1985986e1b6bbfc8e
Rajin <rajink92@gmail.com>
2022-02-24 11:25:35 -0500

    Added for of loop

--- countLetters.js
+++ countLetters.js
@@ -1,7 +1,7 @@
 const countLetters = function(text) {
   const space = text.split(' ').join('');
   const letters = {};
-  for (const letter of space) {
+  for (let letter of space) {
     if (!letters[letter]) letters[letter] = 0;
     letters[letter] += 1;
   }
