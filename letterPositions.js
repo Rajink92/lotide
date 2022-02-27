2e32cb8933219ba060e650d502386ef180b5cac7
Rajin <rajink92@gmail.com>
2022-02-27 16:05:54 -0500

    changed for of loop to normal for loop

--- letterPositions.js
+++ letterPositions.js
@@ -1,17 +1,17 @@
 
 const letterPositions = function(sentence) {
-  const results = {};
-  for (const item of sentence) {
-    const letter = sentence[i];
+  let results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i];
     if (letter !== ' ') {
       if (!Array.isArray(results[letter])) {
-          results[letter] = [i]
+        results[letter] = [i];
       } else {
         results[letter].push(i);
       }
-    }  
+    }
   }
   return results;
 };
 
-module.exports = letterPositions
+module.exports = letterPositions;
--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -2,9 +2,9 @@ const letterPositions = require('../letterPositions');
 const assert = require('chai').assert;
 
 describe("#letter Positions", () => {
-  it("returns 1 for finding e in hello", () => {
+  it("returns [1] for finding e in hello", () => {
     const input = "hello";
-    const expectedOutput = 1;
+    const expectedOutput = [ 1 ];
     assert.deepEqual(letterPositions(input).e, expectedOutput);
   });
 });
