9f54c6555b84f4d187001281c80b4cdfdd0cc980
Rajin <rajink92@gmail.com>
2022-02-27 16:21:43 -0500

    simplifiy test parameter

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -5,6 +5,11 @@ describe("#letter Positions", () => {
   it("returns [1] for finding e in hello", () => {
     const input = "hello";
     const expectedOutput = [ 1 ];
-    assert.deepEqual(letterPositions(input).e, expectedOutput);
+    assert(letterPositions(input), expectedOutput);
+  });
+  it("returns [2] for finding j in rajin", () => {
+    const input = "rajin";
+    const expectedOutput = [2];
+    assert(letterPositions(input), expectedOutput);
   });
 });
