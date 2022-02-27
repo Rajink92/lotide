61f33b8f2ddeffe66f0f520fad32d34a8092b2a1
Rajin <rajink92@gmail.com>
2022-02-27 13:16:02 -0500

    added if statements to for of loop

--- letterPositions.js
+++ letterPositions.js
@@ -2,14 +2,19 @@
 const letterPositions = function(sentence) {
   const results = {};
   for (const item of sentence) {
-    console.log(item);
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
-    }
-}
-return results
-}
+    const letter = sentence[i];
+    if (letter !== ' ') {
+      if (!Array.isArray(results[letter])) {
+
+
+
+    //     if (results[item]) {
+//       results[item] += 1;
+//     } else {
+//       results[item] = 1;
+//     }
+// }
+// return results
+// }
 
 module.exports = letterPositions
